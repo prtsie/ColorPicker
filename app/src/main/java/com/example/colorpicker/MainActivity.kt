27f281968b3aa7ebd29052c4bdec901c68c5e2ae
prtsie <yonhsup@mail.ru>
2025-05-09 11:40:38 +0300

    color picking with button

--- app/src/main/java/com/example/colorpicker/MainActivity.kt
+++ app/src/main/java/com/example/colorpicker/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -28,6 +29,7 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
@@ -49,11 +51,36 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Greeting() {
-    var showDialog by rememberSaveable { mutableStateOf(true) }
+    var showDialog by rememberSaveable { mutableStateOf(false) }
     var red by rememberSaveable { mutableIntStateOf(0) }
     var green by rememberSaveable { mutableIntStateOf(0) }
     var blue by rememberSaveable { mutableIntStateOf(0) }
+    var savedRed by rememberSaveable { mutableIntStateOf(0) }
+    var savedGreen by rememberSaveable { mutableIntStateOf(0) }
+    var savedBlue by rememberSaveable { mutableIntStateOf(0) }
     Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.7f),
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text("#${savedRed.toString(16).padStart(2, '0')}${savedGreen.toString(16).padStart(2, '0')}${savedBlue.toString(16).padStart(2, '0')}")
+                Surface(
+                    modifier = Modifier
+                        .fillMaxWidth(0.8f)
+                        .fillMaxHeight(0.5f),
+                    shape = RoundedCornerShape(30.dp),
+                    color = Color(savedRed, savedGreen, savedBlue)
+                ) { }
+            }
+
+            Button({ showDialog = true }) {
+                Text("Выбрать цвет")
+            }
+        }
         if (showDialog) {
             BasicAlertDialog({}) {
                 Surface(shape = RoundedCornerShape(10.dp)) {
@@ -115,9 +142,16 @@ fun Greeting() {
                                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                                 modifier = Modifier.fillMaxWidth())
                         }
-                        Button({},
+                        Button({
+                            savedRed = red
+                            savedGreen = green
+                            savedBlue = blue
+                            showDialog = false
+                        },
                             colors = ButtonColors(Color(red, green, blue), Color(255 - red, 255 - green, 255 - blue), Color.DarkGray, Color.White),
-                            modifier = Modifier.fillMaxWidth()) {
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 10.dp)) {
                             Text("Применить")
                         }
                     }
